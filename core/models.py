7be4633e4791570b47a50160f09135f6fdbb61fc
apensia914 <apensia914@gmail.com>
2021-03-09 20:07:44 +0900

    Room model structuring #1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+
+    ''' Time Stamped Model ''' 
+
+    created = models.DateTimeField(auto_now_add=True) #4.1 Generate new date & time 
+    updated = models.DateTimeField(auto_now=True) #4.1 Update with new date & time 
+
+    #4.0: Won't be saved in database
+    class Meta: 
+        abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+class Room(core_models.TimeStampedModel):
+
+    ''' Room Model Definition '''
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField() #4.1 Django-countries (https://pypi.org/project/django-countries/)
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    guests = models.IntegerField()
+    instant_book = models.BooleanField(default=False)
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE) #4.1~4.2
