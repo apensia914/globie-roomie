bcb2b201d5f3d26584717ea1103234cd6e41b5a5
apensia914 <apensia914@gmail.com>
2021-03-09 19:46:33 +0900

    Custom user model created

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin #3.5
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    ''' Custom User Admin ''' 
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Profile', {
+            "fields": (
+                'avatar', 'gender', 'bio', 'birthdate', 'language', 'currency', 'superhost',
+            ),
+        }),
+    )
+    
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    ''' Custom User Model '''
+
+    # https://docs.djangoproject.com/en/3.1/ref/models/fields/#django.db.models.Field.choices
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
+
+    LANGUAGE_ENGLISH = 'en'
+    LANGUAGE_KOREAN = 'kr'
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, 'English'),
+        (LANGUAGE_KOREAN, 'Korean'),
+    )
+
+    CURRENCY_USD = 'usd'
+    CURRENCY_KRW = 'krw'
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, 'usd'),
+        (CURRENCY_KRW, 'krw'),
+    )
+
+    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True) #3.2 null: Database, blank: Admin
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3)
+    superhost = models.BooleanField(default=False)
