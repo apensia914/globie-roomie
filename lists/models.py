f19987360558508b280fe6cb3b00282124fbe056
apensia914 <apensia914@gmail.com>
2021-03-10 20:26:31 +0900

    List model structuring

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+class List(core_models.TimeStampedModel):
+
+    ''' List Model Definition ''' 
+
+    name = models.CharField(max_length=80) # Name of the list 
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    rooms = models.ManyToManyField('rooms.Room', blank=True) # Rooms within the list 
+
+    def __str__(self):
+        return self.name
