90210912afeb6561891c07e21669b29666cd0b02
Donghoon Han <apensia914@Donghoons-MacBook-Pro.local>
2021-03-20 16:59:00 +0900

    Login & Logout Completed

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,19 @@
 from django import forms 
+from . import models
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    #14.2 Validating Email
+    def clean_email(self): # In order to check validity, method should always start with "clean_". 
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        try:
+            models.User.objects.get(email=email)
+            if user.check_password(password): 
+                return self.cleaned_data
+            else:
+                self.add_error('password', forms.ValidationError('Password is wrong'))
+        except models.User.DoesNotExist:
+            self.add_error('email', forms.ValidationError('User does not exist'))
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 #14.0 Setting up login form 
 from django.views import View
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout #14.4 
+from django.shortcuts import render, redirect, reverse #14.4 
 from . import forms
 
+#14.1 Login form 
 class LoginView(View):
     def get(self, request):
         form = forms.LoginForm(initial={'email': 'example@example.com'})
@@ -10,4 +12,17 @@ class LoginView(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            #14.4 
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username='email', password='password')
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('core:home'))
         return render(request, 'users/login.html', {'form': form})
+
+#14.4
+def log_out(request):
+    logout(request)
+    return redirect(reverse('core:home'))
